6d4858fa3f3ca671290f4175e42cb01a0ce9f6b8
Daniel Ancuta <danielancuta@dacether.local>
2022-06-01 13:58:26 +0300

    deploy ropsten

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,20 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+
+// console.log(process.env);
 
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.4',
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
   paths: {
-    artifacts: "./src/backend/artifacts",
-    sources: "./src/backend/contracts",
-    cache: "./src/backend/cache",
-    tests: "./src/backend/test"
+    artifacts: './src/backend/artifacts',
+    sources: './src/backend/contracts',
+    cache: './src/backend/cache',
+    tests: './src/backend/test',
   },
 };
